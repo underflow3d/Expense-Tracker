c8e17f890fd49b45730081f95e1e77bcb9a17f72
Aiesh Adnan <developer6383@gmail.com>
2023-05-10 17:18:42 +0000

    Updating Theme Configuration :D

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData(useMaterial3: true),
+    home: const Expenses(),
   ));
 }
