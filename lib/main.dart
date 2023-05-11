9954c62673d70677071aa67680dc8ae58ea89459
Aiesh Adnan <developer6383@gmail.com>
2023-05-11 09:55:37 +0000

    Added New Expense File

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(useMaterial3: true),
     home: const Expenses(),
   ));
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/model/expense.dart';
 
 class Expenses extends StatefulWidget {
@@ -39,7 +40,7 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
         context: context,
         builder: (ctx) {
-          return Text("Bottom Modal Sheet!");
+          return NewExpense();
         });
   }
 
